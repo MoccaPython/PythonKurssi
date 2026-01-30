bb12572960f50a3ba7b2efe1575e81f30046cf98
MoccaPython <tapio.karkkainen@metropolia.fi>
2026-01-30 16:30:49 +0200

    Moduuli 01

--- moduuli 01/exercise 2/teht2_6.py
+++ moduuli 01/exercise 2/teht2_6.py
@@ -1,6 +1,5 @@
 import random
 print("Arvotaan kolminumeroinen (0-9) ja nelinumeroinen luku (1-6)")
-#print(random.randint(1,9), random.randint(1,9), random.randint(1,9))
 kolmi = (random.randint(0,9), random.randint(0,9), random.randint(0,9))
 neli = (random.randint(1,6), random.randint(1,6), random.randint(1,6), random.randint(1,6))
 print(kolmi)
