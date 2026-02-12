bcf7e0dbad8a0b885707b2038dc2d340afda470c
MoccaPython <tapio.karkkainen@metropolia.fi>
2026-02-12 17:52:16 +0200

    Python kurssi

---
+++ Moduuli 04/trht4_6.py
@@ -0,0 +1,11 @@
+#piin likiaron laskeminen
+
+import random
+x = random.randint(-1,1)
+y = random.randint(-1,1)
+
+#maara = int(input("Montako pistettä arvotaan? "))
+
+#while loopit == N:
+ #   if
+  #  loopit = loopit + 1
