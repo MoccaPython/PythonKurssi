22c6abf45af20433ce876599027d3c204c06df3f
MoccaPython <tapio.karkkainen@metropolia.fi>
2026-02-24 08:39:04 +0200

    Python kurssi

--- Moduuli 03/teht3_3.py
+++ Moduuli 03/teht3_3.py
@@ -9,19 +9,19 @@ sex = input("Oletko biologisesti mies vai nainen? m/n ") #Sukupuoli
 if sex == "m" or sex == "M": #onko mies
     hemo = int(input("Anna hemoglobiini arvosi: "))  # hemoglobiini arvo
     if 134 <= hemo <= 195: # oikea hgb taso
-        print("Hieno homma, Hemmo!")
+        print("Hieno homma!")
     elif 134 > hemo: #alhainen hgb taso
-        print("Hemoglobiini alhainen! Syö rautaa tai mene vuoristoon!")
+        print("Hemoglobiini alhainen!")
     elif sex == "m" or sex =="M" and 195 < hemo: #korkea hgb taso
-        print("Hemoglobiinisi on ylhäällä! Laskeudu merenpinnan tasolle")
+        print("Hemoglobiinisi on ylhäällä!")
 elif sex == "n" or sex =="N": #onko nainen
     hemo = int(input("Anna hemoglobiini arvosi: "))  # hemoglobiini arvo
     if 117 <= hemo <= 175:# oikea hgb taso
-        print("Hieno homma, Leidi")
+        print("Hieno homma!")
     elif hemo > 175: # korkea hgb taso
-        print("Hemoglobiinisi on ylhäällä! Laskeudu merenpinnan tasolle")
+        print("Hemoglobiinisi on ylhäällä!")
     elif 117 > hemo: # matala hgb taso
-        print("Hemoglobiini alhainen! Syö rautaa tai mene vuoristoon!")
+        print("Hemoglobiini alhainen!")
 
 else:
     print("En tunnista biologista sukupuoltasi!")
