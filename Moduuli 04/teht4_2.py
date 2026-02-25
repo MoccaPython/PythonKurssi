c81ec7ccc62835a4b36326f5c761e5fedd422c6b
MoccaPython <tapio.karkkainen@metropolia.fi>
2026-02-25 11:25:35 +0200

    Python kurssi

--- Moduuli 04/teht4_2.py
+++ Moduuli 04/teht4_2.py
@@ -1,10 +1,10 @@
 #Muunnetaan tuumat senteiksi!
+
 inch = input("Anna tuumamäärä: ")
 
 tuuma = float(inch)
 
 while (tuuma >= 0):
-    #if tuuma >= 0:
         print(tuuma * 2.54)
         tuuma = float(input("Anna tuumamäärä: "))
 else:
